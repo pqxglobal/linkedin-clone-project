9101b2e5f69129f125307cfbe672c5e83d9b7d97
pqx <l.r.slegt@gmail.com>
2022-07-26 16:48:31 +0200

    Create feed Fixes #6

---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/auth';
+import 'firebase/compat/firestore';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyAsglaOlBF_zNnVYhuAtwntga0OMS7c2kc',
+  authDomain: 'linkedin-clone-ef750.firebaseapp.com',
+  projectId: 'linkedin-clone-ef750',
+  storageBucket: 'linkedin-clone-ef750.appspot.com',
+  messagingSenderId: '881967613670',
+  appId: '1:881967613670:web:fd852984d085e275ef5d27',
+};
+
+firebase.initializeApp(firebaseConfig);
+export const firestore = firebase.firestore();
+
+export default firebase;
