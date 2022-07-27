a350728586befd4a4d7633823950c661e46a0177
pqx <l.r.slegt@gmail.com>
2022-07-27 05:16:23 +0200

    create a login Fixes #8

--- src/App.js
+++ src/App.js
@@ -1,19 +1,47 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './App.css';
 import Header from './Header';
 import Sidebar from './Sidebar';
 import Feed from './Feed';
+import { login, logout, selectUser } from './features/userSlice';
+import { useDispatch, useSelector } from 'react-redux';
+import Login from './Login';
+import { auth } from './firebase';
 
 function App() {
+  const user = useSelector(selectUser);
+  const dispatch = useDispatch();
+  auth.onAuthStateChanged((userAuth) => {
+    if (userAuth) {
+      // user is logged in
+      dispatch(
+        login({
+          email: userAuth.email,
+          uid: userAuth.uid,
+          displayName: userAuth.displayName,
+          photoUrl: userAuth.photoURL,
+        })
+      )
+    } else {
+      // user is logged out
+      dispatch(logout());
+    }
+  });
+  useEffect(() => {}, []);
+
   return (
     <div className="app">
       <Header />
-      {/* App Body */}
-      <div className="app__body">
-        <Sidebar />
-        <Feed />
-        {/* Widgets */}
-      </div>
+
+      {!user ? (
+        <Login />
+      ) : (
+        <div className="app__body">
+          <Sidebar />
+          <Feed />
+          {/* Widgets */}
+        </div>
+      )}
     </div>
   );
 }
--- src/firebase.js
+++ src/firebase.js
@@ -13,5 +13,6 @@ const firebaseConfig = {
 
 firebase.initializeApp(firebaseConfig);
 export const firestore = firebase.firestore();
+export const auth = firebase.auth();
 
 export default firebase;
